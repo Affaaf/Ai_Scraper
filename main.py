f6e51626163f0ec90264fa78de4c2fac639bbc64
Code Graphers <you@example.com>
2023-12-08 14:55:58 +0500

    resolved comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import json
 import logging
 import argparse
+from typing import Dict
 
 
 from constant import Const
@@ -17,7 +18,7 @@ const_obj = Const()
 logging.basicConfig(filename=const_obj.LOGGER_FILENAME, level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def concatenate_dicts(dict1, dict2):
+def concatenate_dicts(dict1: Dict, dict2: Dict) -> Dict:
     """
     Concatenate two dictionaries, combining values for common keys.
 
@@ -51,7 +52,7 @@ def concatenate_dicts(dict1, dict2):
     return result
 
 
-def filter_dict(dictionary):
+def post_process(dictionary: Dict) -> Dict:
     """
     Filter a dictionary to remove entries with values equal to "NOT FOUND" or empty lists.
 
@@ -70,7 +71,7 @@ def filter_dict(dictionary):
     return {key: value for key, value in dictionary.items() if value != "NOT FOUND" and (not isinstance(value, list) or any(item != "NOT FOUND" for item in value))}
 
 
-def scrapper(url):
+def scrapper(url: str) -> None:
     """
     Extract relevant data from HTML content using the OpenAI Chat API and save it to a JSON file.
 
@@ -83,18 +84,15 @@ def scrapper(url):
     Raises:
         json.JSONDecodeError: If there is an error decoding the JSON response from the OpenAI API.
     """
-
     prompt_template = const_obj.PROMPT_TEMPLATE_TEXT
     response = requests.get(url)
     html_content = response.text
    
     CHUNK_SIZE =2000
     prompt_chunks = [html_content[i:i + CHUNK_SIZE] for i in range(0, len(html_content), CHUNK_SIZE)]
-
     scraped_data = {}
 
     for chunk in prompt_chunks:
-        
         chunk = f"Prompt Template: {prompt_template}\n ```Html Content: {chunk}```"
         data = [{"role": "user", "content": chunk}]
         response = openai.ChatCompletion.create(
@@ -110,18 +108,15 @@ def scrapper(url):
             logging.error(f'{const_obj.JSON_ERROR} {e}', exc_info=True)
             continue
        
-        filtered_dict = filter_dict(response_message_dict)
+        filtered_dict = post_process(response_message_dict)
 
         scraped_data = concatenate_dicts(scraped_data, filtered_dict)
     filename = const_obj.JSON_FILENAME
     with open(filename, 'w') as json_file:
         json.dump(scraped_data, json_file)
 
-    return 
-
    
 if __name__ == '__main__':
-  
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', help="The URL of the HTML site.", required=True)
     args = parser.parse_args()
